366a70e52789ec4b5afcb0d9d122d01d10e579b4
S1xela <sixela.raliuga0105@gmail.com>
2022-07-06 00:24:55 -0300

    initial commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+const Index = () => {
+    return (
+        <div className="w-screen h-screen flex justify-center items-center">
+            <div className="text-center">
+                <p className="text-3xl text-stone-900 font-medium">Welcome to <strong className="text-blue-600">Next.JS</strong> + <strong className="text-sky-500">Tailwind.CSS</strong></p>
+                <p className="mt-4 text-xl font-medium">Get started by editing <span className="bg-stone-100 p-2 text-xl font-medium">./src/pages/index.jsx</span></p>
+            </div>
+        </div>
+    )
+}
+
+export default Index
